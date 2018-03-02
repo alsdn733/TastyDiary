6f1ce4857d15d993b78a8301499069a688501ac1
Minwoo <alsdn733@naver.com>
2018-03-03 03:24:36 +0900

    Create toolbar

--- tastydiary/src/main/java/com/example/tastydiary/Main.java
+++ tastydiary/src/main/java/com/example/tastydiary/Main.java
@@ -1,7 +1,10 @@
 package com.example.tastydiary;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
